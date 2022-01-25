119c93df359ae03ecbb03157a83bbc913768cf1e
李韩 <lihan@ecoplants.tech>
2022-01-25 14:31:02 +0800

    modify init

--- deploy/init.sh
+++ deploy/init.sh
@@ -1,8 +1,10 @@
 source /etc/profile
 cd /data/vssh/backend
+python /data/vssh/backend/manage.py migrate
 python /data/vssh/backend/manage.py runserver 0.0.0.0:8000 &
 # 前端编译
 cd /data/vssh/frontend
+npm cache clean --force
+npm install
 npm run serve &
-# npm install
 # npm run build
